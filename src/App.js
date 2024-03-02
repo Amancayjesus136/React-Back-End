143a29ade40d2609d6d711b329b76bdff8580327
Amancayjesus136 <amancayjesus136@gmail.com>
2024-03-02 04:07:20 -0500

    menuUser

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import ShowProducts from './view/Product/ShowProducts';
 import CreateProduct from './view/Product/CreateProduct';
 import EditProduct from './view/Product/EditProduct';
 import MenuUser from './layouts/MenuUser';
+import Dashboard from './view/Home/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={ <MenuUser/> }/>S
+          <Route path='/' element={ <Dashboard/> }/>
+          <Route path='/MenuUser' element={ <MenuUser/> }/>
           <Route path='/show' element={ <ShowProducts/> }/>
           <Route path='/create' element={ <CreateProduct/> }/>
           <Route path='/edit/:id' element={ <EditProduct/> }/>
---
+++ src/view/Home/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuUser from '../../layouts/MenuUser';
+import ShowProducts from '../Product/ShowProducts';
+
+const Dashboard = () => {
+    return (
+        <body>
+            <MenuUser />
+
+        </body>
+    );
+}
+
+export default Dashboard;
